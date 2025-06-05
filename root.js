30f8c86892881a5008c9bc5cfe7510e51b3e513f
amuleo <immohammaddavoodi@gmail.com>
2025-06-05 22:32:10 +0330

    Update root.js

--- root.js
+++ root.js
@@ -1,22 +1,3 @@
-tailwind.config = {
-            darkMode: 'media',
-            theme: {
-                extend: {
-                    colors: {
-                        'base-background': 'var(--color-background)',
-                        'base-text': 'var(--color-text)',
-                        'card-background': 'var(--color-card-background)',
-                        'card-text': 'var(--color-card-text)',
-                        'brand': 'var(--color-brand-accent)',
-                        'border-color': 'var(--color-border-accent)',
-                        'instagram-start': 'var(--color-link-bg-instagram-start)',
-                        'instagram-mid': 'var(--color-link-bg-instagram-mid)',
-                        'instagram-end': 'var(--color-link-bg-instagram-end)',
-                    },
-                },
-            },
-        }
-
 export function getBrandData(brandName) {
     const brandContent = {
         "zixum": {
